841e4bb5011399d081c7b9d5be7972853799599e
XavierEyo <udoeyo91@gmail.com>
2022-06-24 15:18:18 +0100

    Javascript tasks

---
+++ Javascript/Introduction_to_Javascript.js
@@ -0,0 +1,5 @@
+let myName='Eyo Saviour', height='171cm', country='Nigeria';
+
+console.log(myName);
+console.log(height);
+console.log(Nigeria);
---
+++ Javascript/README.md
@@ -0,0 +1,1 @@
+Tasks to test knowledge of Javascript
